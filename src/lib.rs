fd89dee2864d242fc755346a465cbac107492161
Markus Klein <markus-klein@live.de>
2021-09-02 18:43:16 +0200

    Introduce OdbcReader

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,20 @@
+use arrow::datatypes::SchemaRef;
 // Rexport odbc_api and arrow to make it easier for downstream crates to depend to avoid version
 // mismatches
-pub use odbc_api as odbc;
+pub use odbc_api;
 pub use arrow;
 
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+/// Arrow ODBC reader. Implements the [`arrow::record_batch::RecordBatchReader`] trait so it can be
+/// used to fill Arrow arrays from an ODBC data source.
+///
+/// This reader is generic over the cursor type so it can be used in cases there the cursor only
+/// borrows a statement handle (most likely the case then using prepared queries), or owned
+/// statement handles (recommened then using one shot queries, to have an easier life with the
+/// borrow checker).
+pub struct OdbcReader<C> {
+    /// Arrow schema describing the arrays we want to fill from the Odbc data source.
+    pub schema: SchemaRef,
+    /// Odbc cursor with a bound buffer we repeatedly fill with the batches send to us by the data
+    /// source.
+    pub cursor: C
 }
--- tests/integration.rs
+++ tests/integration.rs
@@ -1,16 +1,21 @@
 use std::sync::Arc;
 
-use arrow::{
-    array::Float64Array,
-    datatypes::{DataType, Field, Schema},
-    record_batch::RecordBatch,
-};
 use lazy_static::lazy_static;
-use odbc_api::{
-    buffers::{BufferDescription, BufferKind, ColumnarRowSet, Item},
-    sys::{AttrConnectionPooling, AttrCpMatch},
-    Connection, Cursor, Environment,
+
+use odbc2arrow::{
+    arrow::{
+        array::Float64Array,
+        datatypes::{DataType, Field, Schema},
+        record_batch::RecordBatch,
+    },
+    odbc_api::{
+        buffers::{BufferDescription, BufferKind, ColumnarRowSet, Item},
+        sys::{AttrConnectionPooling, AttrCpMatch},
+        Connection, Cursor, Environment,
+    },
+    OdbcReader,
 };
+
 use stdext::function_name;
 
 /// Connection string to our Microsoft SQL Database. Boot it up with docker-compose up
@@ -31,6 +36,7 @@ lazy_static! {
     };
 }
 
+/// Fill a record batch with non nullable `f64` directly from the datasource
 #[test]
 fn fetch_64bit_floating_point() {
     let table_name = function_name!().rsplit_once(':').unwrap().1;
@@ -56,16 +62,83 @@ fn fetch_64bit_floating_point() {
         nullable: false,
     }];
     let row_set_buffer = ColumnarRowSet::new(max_rows, description.iter().copied());
-    let mut row_set_cursor = cursor.bind_buffer(row_set_buffer).unwrap();
+    let row_set_cursor = cursor.bind_buffer(row_set_buffer).unwrap();
+
+    // Instantiate reader with Arrow schema and ODBC cursor
+    let mut reader = OdbcReader {
+        schema: Arc::new(arrow_schema),
+        cursor: row_set_cursor,
+    };
 
     // Batch for batch copy values from ODBC buffer into values
-    let odbc_batch = row_set_cursor.fetch().unwrap().unwrap();
+    let odbc_batch = reader.cursor.fetch().unwrap().unwrap();
     let column_view = odbc_batch.column(0);
     let slice = f64::as_slice(column_view).unwrap();
     data.extend_from_slice(slice);
 
     let array = Float64Array::from(data);
-    let _arrow_batch = RecordBatch::try_new(Arc::new(arrow_schema), vec![Arc::new(array)]).unwrap();
+    let arrow_batch = RecordBatch::try_new(reader.schema, vec![Arc::new(array)]).unwrap();
+
+    // Assert that the correct values are found within the arrow batch
+    let array_vals = arrow_batch
+        .column(0)
+        .as_any()
+        .downcast_ref::<Float64Array>()
+        .unwrap();
+    assert_eq!([1., 2., 3.], array_vals.values());
+}
+
+/// Like [`fetch_64bit_floating_point`], but utilizing a prepared query instead of a one shot.
+#[test]
+fn prepared_query() {
+    let table_name = function_name!().rsplit_once(':').unwrap().1;
+
+    // Setup a table on the database with some floats (so we can fetch them)
+    let conn = ENV.connect_with_connection_string(MSSQL).unwrap();
+    setup_empty_table(&conn, table_name, &["DOUBLE PRECISION NOT NULL"]).unwrap();
+    let sql = format!("INSERT INTO {} (a) VALUES (1),(2),(3)", table_name);
+    conn.execute(&sql, ()).unwrap();
+
+    // Query column with values to get a cursor
+    let sql = format!("SELECT a FROM {}", table_name);
+    let mut prepared = conn.prepare(&sql).unwrap();
+    let cursor = prepared.execute(()).unwrap().unwrap();
+
+    // Now that we have a cursor, we want to iterate over its rows and fill an arrow batch with it.
+    let arrow_schema = Schema::new(vec![Field::new("a", DataType::Float64, false)]);
+    let mut data = Vec::new();
+
+    // Setup ODBC buffer to bind to cursor
+    let max_rows = 100;
+    let description = [BufferDescription {
+        kind: BufferKind::F64,
+        nullable: false,
+    }];
+    let row_set_buffer = ColumnarRowSet::new(max_rows, description.iter().copied());
+    let row_set_cursor = cursor.bind_buffer(row_set_buffer).unwrap();
+
+    // Instantiate reader with Arrow schema and ODBC cursor
+    let mut reader = OdbcReader {
+        schema: Arc::new(arrow_schema),
+        cursor: row_set_cursor,
+    };
+
+    // Batch for batch copy values from ODBC buffer into values
+    let odbc_batch = reader.cursor.fetch().unwrap().unwrap();
+    let column_view = odbc_batch.column(0);
+    let slice = f64::as_slice(column_view).unwrap();
+    data.extend_from_slice(slice);
+
+    let array = Float64Array::from(data);
+    let arrow_batch = RecordBatch::try_new(reader.schema, vec![Arc::new(array)]).unwrap();
+
+    // Assert that the correct values are found within the arrow batch
+    let array_vals = arrow_batch
+        .column(0)
+        .as_any()
+        .downcast_ref::<Float64Array>()
+        .unwrap();
+    assert_eq!([1., 2., 3.], array_vals.values());
 }
 
 /// Creates the table and assures it is empty. Columns are named a,b,c, etc.
