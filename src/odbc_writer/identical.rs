9873e413723caf3a512aa04ddbb620055f369ffe
Markus Klein <markus-klein@live.de>
2022-06-11 19:11:03 +0200

    generic nullable strategy

--- src/odbc_writer/identical.rs
+++ src/odbc_writer/identical.rs
@@ -1,25 +1,47 @@
-use arrow::array::Int8Array;
-use odbc_api::buffers::{BufferDescription, AnyColumnSliceMut, BufferKind};
+use std::marker::PhantomData;
+
+use arrow::{
+    array::{Array, PrimitiveArray},
+    datatypes::ArrowPrimitiveType,
+};
+use odbc_api::buffers::{AnyColumnSliceMut, BufferDescription, Item};
 
 use crate::WriterError;
 
 use super::WriteStrategy;
 
-pub struct NullableInt8;
+pub struct Nullable<P> {
+    _phantom: PhantomData<P>,
+}
 
-impl WriteStrategy for NullableInt8 {
+impl<P> Nullable<P> {
+    pub fn new() -> Self {
+        Self {
+            _phantom: PhantomData,
+        }
+    }
+}
+
+impl<P> WriteStrategy for Nullable<P>
+where
+    P: ArrowPrimitiveType,
+    P::Native: Item,
+{
     fn buffer_description(&self) -> BufferDescription {
-        BufferDescription { nullable: true, kind: BufferKind::I8 }
+        BufferDescription {
+            nullable: true,
+            kind: P::Native::BUFFER_KIND,
+        }
     }
 
     fn write_rows(
         &self,
         param_offset: usize,
         column_buf: AnyColumnSliceMut<'_>,
-        array: &dyn arrow::array::Array,
+        array: &dyn Array,
     ) -> Result<(), WriterError> {
-        let from = array.as_any().downcast_ref::<Int8Array>().unwrap();
-        let mut to = column_buf.as_nullable_slice::<i8>().unwrap();
+        let from = array.as_any().downcast_ref::<PrimitiveArray<P>>().unwrap();
+        let mut to = column_buf.as_nullable_slice::<P::Native>().unwrap();
         for (index, cell) in from.iter().enumerate() {
             to.set_cell(index + param_offset, cell);
         }
