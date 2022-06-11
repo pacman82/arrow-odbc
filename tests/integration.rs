b0705f0a177ff4adf302125108a369d3ce93ca43
Markus Klein <markus-klein@live.de>
2022-06-11 19:28:52 +0200

    support for inserting int16

--- tests/integration.rs
+++ tests/integration.rs
@@ -942,6 +942,31 @@ fn insert_nullable_int8() {
     assert_eq!(expected, actual);
 }
 
+/// Insert nullable 16Bit Integers into db
+#[test]
+fn insert_nullable_int16() {
+    // Given a table and a record batch reader returning a batch with a text column.
+    let table_name = function_name!().rsplit_once(':').unwrap().1;
+    let conn = ENV.connect_with_connection_string(MSSQL).unwrap();
+    setup_empty_table(&conn, table_name, &["TINYINT"]).unwrap();
+    let schema = Arc::new(Schema::new(vec![Field::new("a", DataType::Int16, true)]));
+    let array1 = Int16Array::from(vec![Some(1), None, Some(3)]);
+    let batch1 = RecordBatch::try_new(schema.clone(), vec![Arc::new(array1)]).unwrap();
+    let reader = StubBatchReader::new(schema, vec![batch1]);
+
+    // When
+    let insert = format!("INSERT INTO {table_name} (a) VALUES (?)");
+    let prepared = conn.prepare(&insert).unwrap();
+    let row_capacity = 5;
+    let mut writer = OdbcWriter::new(row_capacity, reader.schema(), prepared).unwrap();
+    writer.write_all(reader).unwrap();
+
+    // Then
+    let actual = table_to_string(&conn, table_name, &["a"]);
+    let expected = "1\nNULL\n3";
+    assert_eq!(expected, actual);
+}
+
 /// Insert non nullable 8Bit Integers into db
 #[test]
 fn insert_non_nullable_int8() {
